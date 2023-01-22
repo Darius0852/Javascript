adf1162751af81e554a56f4b830b80643aa1b535
Darius Ghomashchian <50425882+Darius0852@users.noreply.github.com>
2023-01-22 15:51:32 +0000

    new example1 added - returns time of day

--- node/example1.js
+++ node/example1.js
@@ -1,9 +1,8 @@
 var http = require('http');
-var url = require('url');
+var dt = require('./myFirstModule');
 
-http.createServer(function(req,res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
-    res.end(txt);
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
 }).listen(8080);
